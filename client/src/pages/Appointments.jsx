bbf56fdebdd3e8e695aa339d60cb91a2c1bdb57b
nellynasir <izzanelly.mn@gmail.com>
2026-05-21 02:35:12 +0800

    Added appointment form validation

--- client/src/pages/Appointments.jsx
+++ client/src/pages/Appointments.jsx
@@ -1,3 +1,5 @@
+import Navbar from "../components/Navbar";
+
 function Appointments() {
     return (
         <div>
--- client/src/pages/BookAppointment.jsx
+++ client/src/pages/BookAppointment.jsx
@@ -1,21 +1,57 @@
+import { useState } from "react";
+import Navbar from "../components/Navbar";
+
 function BookAppointment() {
+    const [date, setDate] = useState("");
+    const [time, setTime] = useState("");
+    const [reason, setReason] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (!date || !time || !reason) {
+            setMessage("Please fill in all fields.");
+            return;
+        }
+
+        if (reason.length < 5) {
+            setMessage("Please elaborate.");
+            return;
+        }
+        setMessage("Appointment form sent!")
+    };
     return (
         <div>
+            <Navbar />
             <hi>Book Appointment</hi>
-            <form>
+            {message && <p>{message}</p>}
+            <form onSubmit={handleSubmit}>
                 <div>
                     <label>Appointment Date</label>
-                    <input type="date" />
+                    <input
+                        type="date"
+                        value={date}
+                        onChange={(e) => setDate(e.target.value)}
+                    />
                 </div>
 
                 <div>
                     <label>Appointment Time</label>
-                    <input type="time" />
+                    <input
+                        type="time"
+                        value={time}
+                        onChange={(e) => setTime(e.target.value)}
+                    />
                 </div>
 
                 <div>
                     <label>Reason for Visit</label>
-                    <textarea placeholder="Enter reason for appointment"></textarea>
+                    <textarea
+                        placeholder="Enter reason for appointment"
+                        value={reason}
+                        onChange={(e)=>setReason(e.target.value)}
+                    ></textarea>
                 </div>
 
                 <button type="submit">Book Appointment</button>
