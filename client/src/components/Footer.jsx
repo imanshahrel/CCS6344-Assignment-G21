8ce1ced4e43653928eab70e0e2d1b864e296ec00
nellynasir <izzanelly.mn@gmail.com>
2026-05-21 23:31:47 +0800

    Move footer styling into CSS

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,6 @@
 function Footer() {
     return (
-        <footer
-            style={{
-                marginTop: "30px",
-                padding: "15px",
-                textAlign: "center",
-            }}
-        >
+        <footer className="footer">
             <p>Clinic Management System © 2026</p>
             <p>Protected using JWT Authentication</p>
         </footer>
